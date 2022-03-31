15f4514d87761544e2ae34c6e9baba3421709ee0
Martin Parunev <parunev@gmail.com>
2022-03-31 15:08:24 +0300

    Add files via upload

---
+++ midExam01/memoryGame.java
@@ -0,0 +1,61 @@
+package important_midExams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class memoryGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> elementsList = Arrays.stream(scanner.nextLine().split(" ")).collect(Collectors.toList());
+        List<String> commandList = Arrays.stream(scanner.nextLine().split(" ")).collect(Collectors.toList());
+
+        int count = 0;
+        int size = elementsList.size();
+
+        while (!commandList.get(0).equals("end")) {
+            count += 1;
+
+            int firstIndex = Integer.parseInt(commandList.get(0));
+            int secondIndex = Integer.parseInt(commandList.get(1));
+
+            if (commandList.get(0).equals(commandList.get(1))
+                    || firstIndex < 0
+                    || firstIndex >= elementsList.size()
+                    || secondIndex < 0
+                    || secondIndex >= elementsList.size()) {
+
+                elementsList.add(elementsList.size() / 2, "-" + count + "a");
+                elementsList.add(elementsList.size() / 2, "-" + count + "a");
+
+                System.out.println("Invalid input! Adding additional elements to the board");
+
+            } else if (elementsList.get(firstIndex).equals(elementsList.get(secondIndex))) {
+                String elementOne = elementsList.get(firstIndex);
+                String elementTwo = elementsList.get(secondIndex);
+
+                elementsList.remove(elementOne);
+                elementsList.remove(elementTwo);
+
+                System.out.printf("Congrats! You have found matching elements - %s!%n", elementOne);
+
+            } else if (elementsList.get(firstIndex) != elementsList.get(secondIndex)) {
+                System.out.println("Try again!");
+            }
+
+            if (elementsList.size() == 0) {
+                System.out.printf("You have won in %d turns!", count);
+                break;
+            }
+            commandList = Arrays.stream(scanner.nextLine().split(" ")).collect(Collectors.toList());
+        }
+        if (commandList.get(0).equals("end")) {
+            System.out.println("Sorry you lose :(");
+            for (String element : elementsList) {
+                System.out.print(element + " ");
+            }
+        }
+    }
+}
